d84ed81e0b608cb9786bce645ad5a4e5ac7c157c
Accurics <admin@accurics.com>
2022-04-18 15:53:11 +0200

    Auto Remediation By Accurics

--- pkg/iac-providers/terraform/v15/testdata/moduleconfigs/ecs/main.tf
+++ pkg/iac-providers/terraform/v15/testdata/moduleconfigs/ecs/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_task_definition" "instanceNotInVpc" {
   family                = "service"
-  network_mode          = "bridge"
+  network_mode          = "awsvpc"
   container_definitions = file("ecs/service.json")
 }
